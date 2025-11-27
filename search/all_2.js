38ed30481a61e7426e4a783287ccf9f130853057
Erix0815 <e.a.erath@gmail.com>
2025-11-27 16:44:17 +0000

    Deploying to gh-pages from @ Erix0815/Datastructures@0d21f7f1cf4f058a6d5b65b1b0f352f2e83f1dd6 🚀

--- search/all_2.js
+++ search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['c_0',['Datastructures (in C)',['../index.html',1,'']]],
-  ['cmp_5ffn_1',['cmp_fn',['../structavl__tree__s.html#ad13bb262cb68bca6403db87e3dd186a6',1,'avl_tree_s::cmp_fn'],['../universal_8h.html#acfd7df959717084d92482f7c740c024c',1,'cmp_fn:&#160;universal.h']]]
+  ['ci_1',['CI',['../index.html#autotoc_md4',1,'']]],
+  ['cmp_5ffn_2',['cmp_fn',['../structavl__tree__s.html#ad13bb262cb68bca6403db87e3dd186a6',1,'avl_tree_s::cmp_fn'],['../universal_8h.html#acfd7df959717084d92482f7c740c024c',1,'cmp_fn:&#160;universal.h']]]
 ];
